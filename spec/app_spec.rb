d08ecabbbec30df7f45077007e289acbc5e1cb12
Tim Pearson <timpearson@deloitte.co.uk>
2017-07-18 08:06:00 +0000

    My Tuesday Update

---
+++ spec/app_spec.rb
@@ -0,0 +1,27 @@
+require File.expand_path '../../server.rb', __FILE__
+require 'rspec'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+describe 'Prototype App' do
+  include Rack::Test::Methods
+
+  def app() Sinatra::Application end
+
+  it 'displays the homepage' do
+    get '/'
+    expect(last_response.status).to eq 200
+  end
+
+it 'displays the company name: Prototype Inc' do
+  get '/'
+  expect(last_response.body).to include "Prototype Inc."
+end
+
+it 'displays the jumbotron title: Best Prototypes' do
+  get '/'
+  expect(last_response.body).to include "Home of the World's Best Prototypes"
+end
+
+end
