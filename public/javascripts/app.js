11cc72876858f6389b2380d3cd148920360cc8b3
Tim Pearson <timpearson@deloitte.co.uk>
2017-07-19 18:42:09 +0000

    Wednesday later work

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,23 @@
-/* global $ */
-
+/*global $ */
 $(document).ready(function(){
-
-  $('#prototype').click(function(event){
-    event.preventDefault();
-    $('#prototype').addClass('active');
-    $('.prototype').removeClass('hidden');
-    $('#about').removeClass('active');
-    $('.about').addClass('hidden');
-  });
   
-  $('#about').click(function(event){
-  event.preventDefault();
-  $('#about').addClass('active');
-  $('.about').removeClass('hidden');
-  $('#prototype').removeClass('active');
-  $('.prototype').addClass('hidden');
-});
+  var tabs = ['contact', 'about', 'prototype'];
+
+  function eventHandler(event){
+    event.preventDefault();
+    tabs.forEach(function(item){
+      if(event.currentTarget.id === item){
+        $('#' + item).addClass('active');
+        $('.'+ item).removeClass('hidden');
+      } else {
+        $('#' + item).removeClass('active');
+        $('.' + item).addClass('hidden');
+      }
+    });
+  }
+
+  $('#prototype').click(eventHandler);
+  $('#about').click(eventHandler);
+  $('#contact').click(eventHandler);
 
 });
